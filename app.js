379f43e24aebff69a1b65ec8e36f589740e5f04d
Shubhankar <shubhosen98@gmail.com>
2021-08-26 16:40:12 +0530

    Made major changes to the project

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const anchors = document.querySelectorAll('a');
-const li = document.querySelectorAll('.dropdown');
 const img = document.querySelector('.alternate-img');
 const img1 = document.querySelector('.alternate-img1');
+const navbarToggle = document.querySelector('.navbar-toggler-icon');
+const paragraph = document.querySelectorAll('.text-main-nav');
+const specialAnchors = document.querySelectorAll('#navbarDropdownMenuLink');
+
 
 for (let i of anchors) {
-    i.addEventListener('click', (evt) => {
-        evt.preventDefault();
+    i.addEventListener('click', evt => {
+        evt.preventDefault(); // it prevents the default click event features.
     })
 };
 
@@ -16,7 +19,7 @@ function defaultCheck() {
         img.src = 'file:///F:/FrontEndCodeChallege3/images/illustration-editor-mobile.svg';
         img1.src = 'file:///F:/FrontEndCodeChallege3/images/illustration-laptop-mobile.svg';
     }
-    
+
     else {
         img.src = 'file:///F:/FrontEndCodeChallege3/images/illustration-editor-desktop.svg';
         img1.src = 'file:///F:/FrontEndCodeChallege3/images/illustration-laptop-desktop.svg';
@@ -30,3 +33,24 @@ defaultCheck();
 window.onresize = function checkWidthScreen() {
     defaultCheck();
 };
+
+
+function toggle(evt) {
+
+    evt.target.classList.toggle('toggle-img');
+
+    for (let para of paragraph) {
+        para.classList.toggle('hidden');
+    }
+}
+
+navbarToggle.addEventListener('click', toggle);
+
+
+specialAnchors.forEach((navLiAnchor) => {
+    navLiAnchor.addEventListener('click', evt => {
+
+        navLiAnchor.classList.toggle('nav-toggle-icon-move-sp');
+    })
+
+})
